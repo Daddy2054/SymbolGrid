30bbed1fd44b4aca3086c6c9bf28d17413465dc9
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-11 23:11:53 +0200

    final

---
+++ SymbolGrid/Symbol.swift
@@ -0,0 +1,14 @@
+//
+//  Symbol.swift
+//  SymbolGrid
+//
+//  Created by Jean on 11/10/24.
+//
+
+import SwiftUI
+
+
+struct Symbol : Identifiable, Equatable {
+    var id = UUID()
+    var name : String
+}
--- SymbolGrid/SymbolGridApp.swift
+++ SymbolGrid/SymbolGridApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct SymbolGridApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                SymbolGrid()
+            }
         }
     }
 }
