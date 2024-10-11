9022e74932e146c89014439273b52b23324fdc2e
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-11 22:31:54 +0200

    Initial Commit

---
+++ SymbolGrid/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridApp.swift
+//  SymbolGrid
+//
+//  Created by Jean on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
